25c900515860cce5025a39331c07013b8c482d81
Loi Nguyen <loi.nguyen@friartuck.net>
2021-11-12 16:46:32 +0800

    commit

--- src/main/java/creational/singleton/Singleton2.java
+++ src/main/java/creational/singleton/Singleton2.java
@@ -1,11 +1,27 @@
 package creational.singleton;
 
 public class Singleton2 {
+    static {
+        System.out.println("Singleton2 initializing...");
+    }
+
     public static class SingletonHolder {
+        private SingletonHolder() {
+
+        }
+
+        static {
+            System.out.println("SingletonHolder initializing...");
+        }
         private static final Singleton2 instance = new Singleton2();
     }
 
     public static Singleton2 getInstance() {
+        System.out.println("Singleton2 getInstance....");
         return SingletonHolder.instance;
+    }
+
+    public void doSomething() {
+        //blah blah....
     }
 }
--- src/main/java/creational/singleton/Test.java
+++ src/main/java/creational/singleton/Test.java
@@ -3,5 +3,7 @@ package creational.singleton;
 public class Test {
     public static void main(String[] args) {
         System.out.println(Singleton.getInstance());
+
+        System.out.println(Singleton2.getInstance());
     }
 }
